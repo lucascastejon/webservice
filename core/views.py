32ba51342159c9a43e77b7932e61b116dcd4eac1
Lucas Castejon <lucascastejon@gmail.com>
2014-09-20 21:09:21 -0300

    Added folder of the documentation

--- core/views.py
+++ core/views.py
@@ -8,13 +8,12 @@ HOST = 'http://192.168.19.18:8080/'
 def function():
   #### TESTE HOST
   print("Recebendo dados..")
-  clientes = urllib.request.urlopen('http://192.168.19.18:8080/WS_FastLine/cliente_teste/listarTodos')
+  clientes = urllib.request.urlopen(HOST + '/WS_FastLine/cliente_teste/listarTodos')
 
-  # clientes = urllib.request.urlopen(str(HOST))
   clientes = json.loads(str(clientes.read().decode('utf-8')))
   clientes = clientes["clienteTeste"]
   
-  ##### TESTE LOCAL
+  ##### TESTE LOCAL #####
   # clientes = json.loads('{"clientesTest":[{"id_cliente": "1","nm_cliente": "Lucas Castejon Alves","numero_cel": "993353325","email": "lucascastejon@gmail.com","cep": "14403180"},{"id_cliente": "2","nm_cliente": "Michel Ribeiro","numero_cel": "991293942","email": "michelribeiro@gmail.com","cep": "14403440"},{"id_cliente": "3","nm_cliente": "Leandro Borgesss","numero_cel": "999294123","email": "leandro@borges.br","cep": "14403180"}] }')
   # clientes = clientes["clientesTest"]
   return clientes
@@ -55,11 +54,8 @@ def salvar_enviar(request):
   email      = request.GET['email']
   cep        = request.GET['cep']
 
-
   # CONCATENANDO PARÂMETROS A SER ENVIADO VIA URL
   dados = id_cliente+'/'+nm_cliente.replace(" ","%20")+'/'+numero_cel.replace(" ","%20")+'/'+email+'/'+cep
-
-
   save = urllib.request.urlopen(HOST+'WS_FastLine/cliente_teste/update/'+dados)
   
   clientes   = function()
